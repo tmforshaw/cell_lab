81b362b90ff2d3e4fc0b2f4391d6c0d8a88bc313
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2026-03-28 02:31:49 +0000

    Seperate keyboard input by game mode

--- src/input.rs
+++ src/input.rs
@@ -2,15 +2,26 @@ use bevy::{input::keyboard::KeyboardInput, prelude::*};
 
 use crate::state::GameMode;
 
-pub fn keyboard_event_system(
+pub fn keyboard_event_system_play_mode(mut events: MessageReader<KeyboardInput>, mut next_mode: ResMut<NextState<GameMode>>) {
+    for ev in events.read() {
+        // TODO
+        #[allow(clippy::single_match)]
+        match ev.key_code {
+            KeyCode::Digit2 => next_mode.set(GameMode::CellEditor),
+            _ => {}
+        }
+    }
+}
+
+pub fn keyboard_event_system_cell_editor_mode(
     mut events: MessageReader<KeyboardInput>,
     mut next_mode: ResMut<NextState<GameMode>>,
-    mode: Res<State<GameMode>>,
 ) {
     for ev in events.read() {
+        // TODO
+        #[allow(clippy::single_match)]
         match ev.key_code {
-            KeyCode::Digit1 if !mode.eq(&GameMode::Play) => next_mode.set(GameMode::Play),
-            KeyCode::Digit2 if !mode.eq(&GameMode::CellEditor) => next_mode.set(GameMode::CellEditor),
+            KeyCode::Digit1 => next_mode.set(GameMode::Play),
             _ => {}
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 use crate::{
     cell::{bound_cells, cell_decay, cells_absorb_chemical, cells_do_meiosis, move_cells},
     chemical::{ChemicalTimer, spawn_chemicals},
-    input::keyboard_event_system,
+    input::{keyboard_event_system_cell_editor_mode, keyboard_event_system_play_mode},
     state::{GameMode, GameState, exit_cell_editor_mode, exit_play_mode, init_cell_editor_mode, init_play_mode},
 };
 
@@ -17,16 +17,17 @@ pub mod state;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .init_state::<GameMode>()
+        // .init_state::<GameMode>()
+        .insert_state(GameMode::CellEditor)
         .init_resource::<GameState>()
         .init_resource::<ChemicalTimer>()
         .add_systems(Startup, setup)
-        .add_systems(Update, keyboard_event_system)
         // ---------------------------- Play Mode -----------------------------
         .add_systems(OnEnter(GameMode::Play), init_play_mode)
         .add_systems(
             Update,
             (
+                keyboard_event_system_play_mode,
                 spawn_chemicals,
                 move_cells,
                 bound_cells,
@@ -39,17 +40,10 @@ fn main() {
         .add_systems(OnExit(GameMode::Play), exit_play_mode)
         // ------------------------- Cell Editor Mode --------------------------
         .add_systems(OnEnter(GameMode::CellEditor), init_cell_editor_mode)
-        // .add_systems(
-        //     Update,
-        //     (
-        //         spawn_chemicals,
-        //         move_cells,
-        //         bound_cells,
-        //         cells_absorb_chemical,
-        //         cells_do_meiosis,
-        //     )
-        //         .run_if(in_state(GameMode::CellEditor)),
-        // )
+        .add_systems(
+            Update,
+            (keyboard_event_system_cell_editor_mode,).run_if(in_state(GameMode::CellEditor)),
+        )
         .add_systems(OnExit(GameMode::CellEditor), exit_cell_editor_mode)
         // ---------------------------------------------------------------------
         .run();
